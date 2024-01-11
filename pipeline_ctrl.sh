648f7816fa0f5b84112e0da655c7467443edc901
Tom Hill <tom.hill@ku.edu>
2024-01-11 09:34:03 -0500

    Add files via upload

---
+++ pipeline_ctrl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+module load python
+module load snakemake/5
+#module load snakemake
+
+R=$2
+
+mkdir -p ${R}/snakejobs
+mkdir -p $R/Reports
+
+if [ $1 == "npr" ]
+then
+    snakemake --unlock --snakefile $R/Snakefile -j 1 --configfile $R/config.yaml
+    snakemake -npr --snakefile $R/Snakefile -j 1 --configfile $R/config.yaml
+fi
+
+if [ $1 == "process" ]
+then
+### WORKING
+snakemake --unlock --snakefile $R/Snakefile -j 1 --configfile $R/config.yaml
+snakemake --latency-wait 120 --configfile $R/config.yaml -s $R/Snakefile -d $R --printshellcmds --use-conda --cluster-config $R/cluster.json --keep-going --cluster "sbatch --gres {cluster.gres} --cpus-per-task {cluster.threads} -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} --job-name={params.rname} -e snakejobs/slurm-%j_{params.rname}.out -o snakejobs/slurm-%j_{params.rname}.out" -j 500 --rerun-incomplete --stats $R/Reports/snakemake.stats | tee -a $R/Reports/snakemake.log
+fi
